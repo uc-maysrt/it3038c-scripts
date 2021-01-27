7203ceb924e499a5d7885507c67a23161641b231
Robert <maysrt@mail.uc.edu>
2021-01-26 19:39:49 -0500

    Lab3 files

---
+++ lab3/sendemail.ps1
@@ -0,0 +1,21 @@
+﻿function getIP{
+	(get-netipaddress).ipv4address | Select-String "192*"
+}
+function getHostname{
+	(Get-ComputerInfo).CsName | Select-String "maysrt-win"
+}
+function getPsversion{
+	($HOST.major)
+}
+
+$IP = getIP
+$hostname = gethostname
+$psversion = getPsversion
+$date = get-Date
+
+
+
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $hostname. PowerShell Version $psversion. Today's date is $date"
+Write-Host("$Body")
+
+#Send-MailMessage -To "maysrt@mail.uc.edu" -From "maysrt@mail.uc.edu" -Subject "IT3038C Windows SysInfo" -Body $Body -SmtpServer smtp.google.com -port 587 -UseSSL -Credentials (Get-Credential)
