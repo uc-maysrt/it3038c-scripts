ba9dc0c7c34cc28eeeb079fcd7008d3f23c06fbb
Robert <maysrt@mail.uc.edu>
2021-02-14 07:53:31 -0500

    Lab5.py initial commit

---
+++ python/Lab5.py
@@ -0,0 +1,12 @@
+import time
+
+#Take a birthday input and calculate how many seconds old you are
+
+#Take a word as input and count how many letters, how many vowels, and how many consonants
+
+#Take a number input and calculate how many prime numbers come between it and 0
+
+#Write a 'guess the number game' where a random number is generated and the user must guess the number.
+##The program says if their number is too high or too low until the right answer is guessed.
+
+correctNumber = random()
