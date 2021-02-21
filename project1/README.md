27c629e1d7282e983274bcfd1a75415ab83bbfa5
Robert <maysrt@mail.uc.edu>
2021-02-21 18:04:02 -0500

    readme updates. start of file work.

--- project1/README.md
+++ project1/README.md
@@ -1,5 +1,9 @@
 # Project 1
-This program bulk creates and moves files based on user input.  I created it because I have a large amount of files that I would like to be more organized based on the numeric sequence they are in.
+This program bulk creates and moves files based on user input.
+I created it because I have a large amount of files that I would like to be more organized based on the numeric sequence they are in.  These files are created by loading a video into a program called [VirtualDub2](https://sourceforge.net/projects/vdfiltermod/) and then doing an image sequence.
+Included in the project1 folder are test files from a Kaltura video made for an English class. Example Output below is based on the Kaltura video, not the actual project. Due to github free account storage limitations, not all of the files in the test folder (storage limitation is 1GB, test file output is 1.33GB)
+
+
 To run this program, make sure Python3 is installed and working.
 
 If using Windows, type:  
@@ -16,23 +20,23 @@ Example Output:
 ```
 $python bulkfilemover.py
 How many files do you have?
-70000
+10584
 How many folders would you like?
-70
+11
 How many files per folder would you like?
 1000
 Where is the parent directory located?
 C:\project1
 What would you like the folders called? folder-
-Folder folder-1000 created
-Folder folder-2000 created
+Folder folder-1 created
+Folder folder-2 created
 ...
-Folder folder-70000 created
+Folder folder-11 created
 
 What is the filename prefix? filename
 What is the file extension? .jpg
-filename(1-1000).jpg moved into folder1000
-filename(1001-2000).jpg moved into folder2000
+filename(1-1000).jpg moved into folder-1
+filename(1001-2000).jpg moved into folder-2
 ...
-filename(69001-70000).jpg moved into folder70000
+filename(10001-10584).jpg moved into folder-11
 ```
--- project1/bulkfilemover.py
+++ project1/bulkfilemover.py
@@ -1,7 +1,6 @@
 import os
-#This version will work on Windows but not Linux/Mac [yet]
-#os.system commands are meant to be platform agnostic and
-#technically runs in Batch not powershell
+#I have only tested this on Windows since that is how I create the source files of the project.
+#In theory, the os.* commands should be plaform agnostic and it will work on Linux/Mac
 
 #gets some user input for later
 print("How many files do you have?")
@@ -14,25 +13,27 @@ print("Where is the parent directory located?")
 parentDir = input()
 print("What would you like the folders called?")
 folderPrefix = input()
-print("What is the filename prefix?")
-filenamePrefix = input()
-print("What is the filename extension?")
-fileExtension = input()
-
-#probably not needed?
-#print("What is the first filename?")
-#firstFile = input()
-#print("What is the last filename?")
-#lastFile = input()
-
 #creates folders
 # runs through a while loop until there are no new folders to create.
 while 0 < numFolders:
     folderName = folderPrefix + str(numFolders)
+    #Not really reused from another source but:
+    # https://docs.python.org/3/library/os.html for finding the os.commands
+    # https://www.geeksforgeeks.org/python-os-mkdir-method/ for more clarification, also the parentDir idea
     newPath = os.path.join(parentDir,folderName)
     os.mkdir(newPath)
     print("Folder " + folderName + " created")
-    #counts down the number
+    #counts down the number. https://datatofish.com/while-loop-python/
+    # sort of for the source because I had forgotten to count down originally...
     numFolders = numFolders - 1
 #moves files into previously created folders based on filenames/user input
+print("What is the filename prefix?")
+filenamePrefix = input()
+print("What is the filename extension?")
+fileExtension = input()
+#runs through a while loop until there are no more files to move.
+while 0 < numFiles:
+    os.(something/to/make/the/files/move/to/dir)
+    print("File " + filevariableundefinedatthemoment + " has been moved to " + folderNameGoesHere)
+    numFiles = numFiles - 1
 #os.sys("")
