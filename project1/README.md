1348416f96553593c92fad806a634af9050dfda2
Robert <maysrt@mail.uc.edu>
2021-02-21 23:24:13 -0500

    moved project1 development over to here.

---
+++ project1/README.md
@@ -0,0 +1,53 @@
+# Project 1
+
+This program bulk creates folders and moves files based on user input.
+I wrote it because I have a large amount of files that I would like to be more organized based on the numeric sequence they are in.  These files are created by loading a video into a program called [VirtualDub2](https://sourceforge.net/projects/vdfiltermod/) and then doing an image sequence.
+
+  Included in the project1 folder are test files from a Kaltura video made for an English class. Example Output below is based on the first 1000 frames from the test video, not the actual project or the total Kaltura video due to file storage limitations listed below. 
+
+  Due to github free account storage limitations, not all of the files in the `testfiles` folder (storage limitation is 1GB, test file output is 1.33GB).  I have included the first 1000 files (73MB).  
+  For comparison's sake, the actual project is a 39m26s video of 71,302 frames and the image file total size is 38.5GB.
+
+To run this program, make sure Python3 is installed and working.  
+
+  If using Windows, type:  
+  ```powershell
+  python --version
+  ```  
+
+  If using Linux, you may need to specify Python3:  
+  ```bash
+  python3 --version
+  ```
+
+
+Usage instructions
+1. copy the files located in `example files` to a directory of your choosing
+2. From the project1 folder that contains bulkfilemover.py, run the program using Python  
+```python bulkfilemover.py```
+3. Follow the instructions on screen.
+
+Example Test Output:
+```
+$python bulkfilemover.py
+How many folders would you like?
+10
+Where is the parent directory located? Include trailing / or \
+C:\project1\
+What would you like the folders called? example-
+Folder example-01 created
+Folder example-02 created
+...
+Folder example-10 created
+
+What is the last filename number? 1000
+example-(00001-00100).jpeg moved into example-01
+example-(00101-00200).jpeg moved into example-02
+...
+example-(00901-01000).jpeg moved into example-10
+```
+
+## Known issues
+1. Amount of files per directory is hardcoded to 100.
+2. Linux (and possibly Mac): example-00999.jpeg and example-01000.jpeg do not move over. This behavior does not happen on Windows.
+3. Really only works with the example files and not the actual project files that I have. [this is due to the hardcode of 100 per folder]
---
+++ project1/bulkfilemover.py
@@ -0,0 +1,123 @@
+import os
+import shutil
+#I have only tested this on Windows since that is how I create the source files of the project.
+#In theory, the os.* and shutil commands should be plaform agnostic and it will work on Linux/Mac
+
+#gets some user input for later
+
+# This is unnecessary because the numbers per folder are hardcoded for now
+#print("How many files do you have?")
+#numFiles = int(input())
+
+print("How many folders would you like?")
+numFolders = int(input())
+
+# This is unnecessary because the number of files per folder are hardcoded for now
+#print("How many files per folder would you like?")
+#numFilesFolder = input()
+
+#Since Windows does \ but Mac+Linux does /
+#added the prompt about add the trailing slash
+print("Where is the parent directory located? Include trailing / or \\")
+parentDir = input()
+print("What would you like the folders called?")
+folderPrefix = input()
+
+#throws the file listing into a list to call later.
+# discovered at https://www.tutorialspoint.com/python/os_listdir.htm 
+fileList = os.listdir( parentDir )
+#initialize an empty list that will be added to during the while loop.
+directoryList = []
+
+#creates folders
+# runs through a while loop until there are no new folders to create.
+while 0 < numFolders:
+    if numFolders == 10:
+        folderName = folderPrefix + str(numFolders)
+    #Not really reused from another source but:
+    # https://docs.python.org/3/library/os.html for finding the os.commands
+    # https://www.geeksforgeeks.org/python-os-mkdir-method/ for more clarification, also the parentDir idea
+        newPath = os.path.join(parentDir,folderName)
+        os.mkdir(newPath)
+        print("Folder " + folderName + " created")
+    #Throwing the directory into a list for later recall
+        directoryList.append(newPath)
+    #counts down the number. https://datatofish.com/while-loop-python/
+    # sort of for the source because I had forgotten to count down originally...
+        numFolders = int(numFolders) - 1
+    else:
+    #I really wanted my leading "0" if the number was under 10...
+        folderName = folderPrefix + "0" + str(numFolders)
+    #Not really reused from another source but:
+    # https://docs.python.org/3/library/os.html for finding the os.commands
+    # https://www.geeksforgeeks.org/python-os-mkdir-method/ for more clarification, also the parentDir idea
+        newPath = os.path.join(parentDir,folderName)
+        os.mkdir(newPath)
+        print("Folder " + folderName + " created")
+    #Throwing the directory into a list for later recall
+        directoryList.append(newPath)
+    #counts down the number. https://datatofish.com/while-loop-python/
+    # sort of for the source because I had forgotten to count down originally...
+        numFolders = numFolders - 1
+
+#moves files into previously created folders based on filenames/user input
+# None of these were necessary
+# print("What is the filename prefix?")
+# filenamePrefix = input()
+# print("What is the filename extension?")
+# fileExtension = input()
+# print("What is the first filename number?")
+# startFileNum = input()
+# print("How many leading zeroes? Ex: 0000")
+# leadingZeroes = input()
+
+# this one is necessary for the while loop.
+print("What is the last filename number?")
+lastFileNum = int(input())
+
+# initializing the variable for later.
+fileCount = 0
+#runs through a while loop until there are no more files to move.
+#Discovered the main command through: https://docs.python.org/3/library/shutil.html
+ 
+while fileCount != lastFileNum:
+#was a double conditional while loop. the first condition has been rendered unnecessary for the time being.
+# first condition was "0 < numFiles" Runs if the number of files is > 0
+# second/currently only condition runs as long as fileCount does not equal the last file number put in by the user
+# should add error checking.
+
+#Because of how the directoryList list was constructed, it iterates through the list backwards.
+    if fileCount < 100:
+        shutil.move(parentDir + fileList[fileCount],directoryList[9])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[9])
+    elif 100 <= fileCount <= 199:
+        shutil.move(parentDir + fileList[fileCount],directoryList[8])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[8])
+    elif 200 <= fileCount <= 299:
+        shutil.move(parentDir + fileList[fileCount],directoryList[7])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[7])
+    elif 300 <= fileCount <= 399:
+        shutil.move(parentDir + fileList[fileCount],directoryList[6])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[6])
+    elif 400 <= fileCount <= 499:
+        shutil.move(parentDir + fileList[fileCount],directoryList[5])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[5])
+    elif 500 <= fileCount <= 599:
+        shutil.move(parentDir + fileList[fileCount],directoryList[4])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[4])
+    elif 600 <= fileCount <= 699:
+        shutil.move(parentDir + fileList[fileCount],directoryList[3])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[3])
+    elif 700 <= fileCount <= 799:
+        shutil.move(parentDir + fileList[fileCount],directoryList[2])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[2])
+    elif 800 <= fileCount <= 899:
+        shutil.move(parentDir + fileList[fileCount],directoryList[1])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[1])
+    elif 900 <= fileCount <= 999:
+        shutil.move(parentDir + fileList[fileCount],directoryList[0])
+        print("File " + fileList[fileCount] + " has been moved to " + directoryList[0])
+#counts the number of files down by one ->rendered unnecessary
+#    numFiles = numFiles - 1
+    #counts files moved up by one
+    fileCount = fileCount + 1
