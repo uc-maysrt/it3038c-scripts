7b0ddb9e7573f2dcc5da95527b6202f2fd12550e
Robert <maysrt@mail.uc.edu>
2021-02-21 20:35:35 -0500

    Moving back downstairs

--- project1/bulkfilemover.py
+++ project1/bulkfilemover.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 #I have only tested this on Windows since that is how I create the source files of the project.
 #In theory, the os.* commands should be plaform agnostic and it will work on Linux/Mac
 
@@ -26,14 +27,28 @@ while 0 < numFolders:
     #counts down the number. https://datatofish.com/while-loop-python/
     # sort of for the source because I had forgotten to count down originally...
     numFolders = numFolders - 1
+
 #moves files into previously created folders based on filenames/user input
 print("What is the filename prefix?")
 filenamePrefix = input()
 print("What is the filename extension?")
 fileExtension = input()
+print("What is the first filename number?")
+startFileNum = input()
+print("What is the last filename number?")
+lastFileNum = input()
+print("How many leading zeroes? Ex: 0000")
+leadingzeroes = input()
+#initializing the variable for later.
+fileCount = 0
 #runs through a while loop until there are no more files to move.
-while 0 < numFiles:
-    os.(something/to/make/the/files/move/to/dir)
+#Discovered the main command through: https://docs.python.org/3/library/shutil.html
+# 
+while 0 < numFiles and fileCount != lastFileNum:
+#double conditional while loop.  Runs if the number of files is > 0 and as long as <variable> != lastFileNum
+    if 0 < 
     print("File " + filevariableundefinedatthemoment + " has been moved to " + folderNameGoesHere)
+#counts the number of files down by one
     numFiles = numFiles - 1
-#os.sys("")
+#counts files moved up by one
+    fileCount = fileCount + 1
